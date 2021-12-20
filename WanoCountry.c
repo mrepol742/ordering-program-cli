a2b6793cfbbdfd89e3d726e1c9b003cc5a436890
Melvin Jones Repol <mrepol742@gmail.com>
2021-12-20 23:57:48 +0800

    Initial Commit

--- WanoCountry.c
+++ WanoCountry.c
@@ -3,15 +3,27 @@
 int main() {
     printf("[Welcome to Wano Country Restaurant]\n\n");
     printf("Menu:\n");
-    printf("1. 59₱ Boiled Chicken  2. 89₱ Sashimi\n3. 99₱ Takoyaki        4. 59₱ Mochi\n5. 79₱ Onigiri         6. 99₱ Ramen\n7. 99₱ Samba Beefs     8. 79₱ Bento\n9. 59₱ Oden            0. 99₱ Chopper\n\n");
-    int prices[] = {59, 89, 99, 59, 99, 99, 79, 59, 99};
-    prinf("\n\nEnter your order:");
+    char order[][20] = {"Boiled Chicken", "Sashimi", "Takoyaki", "Mochi", "Onigiri", "Ramen", "Samba Beefs", "Bento", "Oden", "Chopper"};
+    printf("1. 59₱ Boiled Chicken  2. 89₱ Sashimi\n3. 99₱ Takoyaki        4. 59₱ Mochi\n5. 79₱ Onigiri         6. 99₱ Ramen\n7. 99₱ Samba Beefs     8. 79₱ Bento\n9. 59₱ Oden            10. 99₱ Chopper\n\n");
+    int prices[] = {59, 89, 99, 59, 79, 99, 99, 79, 59, 99};
+    printf("\n\nEnter your order: ");
     int firstOrder;
     scanf("%d", &firstOrder);
-    printf("\nEnter your second order:");
+    printf("Enter your second order: ");
     int secondOrder;
     scanf("%d", &secondOrder);
-    int sum = firstOrder + secondOrder;
-    printf("\n\nYour receipt: ");
+    int sum = prices[firstOrder-1] + prices[secondOrder-1];
+    printf("\nYour receipt:");
+    int i;
+    for (i = 0; i < 10; i++) {
+        if (i == (firstOrder-1)) {
+            printf("\n%d₱ %s", prices[i], order + i);
+        }
+        if (i == (secondOrder-1)) {
+            printf("\n%d₱ %s", prices[i], order + i);
+        }
+    }
+    printf("\n________\n%d Total Amount\n", sum);
+    
     return 0;
 }
