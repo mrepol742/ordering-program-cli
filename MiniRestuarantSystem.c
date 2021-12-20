de4bf24272acf7987b03126413a1452b81ebd100
Melvin Jones Repol <mrepol742@gmail.com>
2021-12-20 19:05:45 +0800

    Initial Commit

---
+++ MiniRestuarantSystem.c
@@ -0,0 +1,76 @@
+#include <stdio.h>
+#include <time.h>
+
+void delay(int ms);
+void menu();
+void input();
+void receipt();
+
+char food[][20] = {"Lemon Pie", "Iced Coffee", "Pork Cutlet Bowl", "Takiyaki", "Yaki Dango", "Ramen", "Onigiri", "Curry", "Pocky"};
+int price[10] = {59, 49, 99, 79, 89, 59, 99, 59, 79};
+int orders[99];
+
+int main() {
+    printf("--- FAnime Restuarant ---\n\n");
+    delay(500);
+    printf("Welcome to our restuarant!\n");
+    delay(500);
+    printf("Here's our menu:\n");
+    delay(500);
+    menu();
+    printf("\nEnter the number you like to order: ");
+    delay(500);
+    input();
+    
+    return 0;
+}
+
+int length(int *p) { 
+	int s = 0; 
+	while(*p != '\0') { 
+		s++; 
+		p++; 
+	} 
+	return s; 
+} 
+
+void receipt() {
+    int sum;
+    for (int i = 0; i < 9; i++) {
+        sum += orders[i] + i;
+        printf("%d", orders[i]);
+    }
+    printf("\n\nThe total amount is %d", sum);
+}
+
+void input() {
+    // update do while loop
+    scanf("%d", &orders[0]);
+    printf("Anything else? ");
+    // if 0 reciept
+    if (orders[length(&orders[0]- 1)] == 0) {
+       receipt();
+    } else {
+       input();
+    }
+}
+
+void menu() {
+    int i;
+    for (i = 0; i < 9; i++) {
+        // TODO: warning format %s expects the argument of type char *
+        printf("%d. %s - %dP\n", i + 1, food + i, price[i]);
+        delay(250);
+    }
+    printf("Enter '0' to finished the order.\n");
+}
+
+void delay(int ms) {
+    long pe;
+    clock_t now, then;
+    pe = ms * (CLOCKS_PER_SEC/1000);
+    now = then = clock();
+    while((now-then) < pe) {
+        now = clock();
+    }
+}
