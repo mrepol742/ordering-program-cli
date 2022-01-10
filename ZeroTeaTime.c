bcc68ccd2e87121278249bbe39b9bf00b48e8656
Melvin Jones Repol <mrepol742@gmail.com>
2022-01-10 19:47:10 +0800

    Initial Commit

--- ZeroTeaTime.c
+++ ZeroTeaTime.c
@@ -1,6 +1,13 @@
+/*
+*
+* Copyright (c) 2022 Melvin Jones Repol (mrepol742.github.io). All rights reserved.
+*
+*/
+
 #include <stdio.h>
 #include <time.h>
 
+int sum(int *orders, int length);
 void delay(int ms);
 void menu();
 void input();
@@ -27,6 +34,7 @@ int main() {
 
 int length(int *p) { 
 	int s = 0; 
+    // eren
 	while(*p != '\0') { 
 		s++; 
 		p++; 
@@ -35,29 +43,50 @@ int length(int *p) {
 } 
 
 void receipt() {
-    int sum;
-    for (int i = 0; i < 9; i++) {
-        sum += orders[i] + i;
-        printf("%d", orders[i]);
+    int i, sum = 0;
+    for (i = 0; i < length(orders); i++) {
+        sum += price[orders[i]];
+        // debug
+       // printf("\ncurrent price added is %d", price[orders[i]]);
+    } 
+    printf("\n\nThe total amount is: %d\n", sum);
+}
+
+int concat(int x, int y) {
+    int temp = y;
+    while (y != 0) {
+        x *= 10;
+        y /= 10;
     }
-    printf("\n\nThe total amount is %d", sum);
+    return x + temp;
+}
+
+int sum(int *count, int length) {
+   int total = 0;
+   int i;
+   for (i = 0; i < length; i++) {
+        total += count[i];
+   }
+   return (total);
 }
 
 void input() {
     int i;
     while (2 < 3) {
+        i++;
         int or;
         scanf("%d", &or);
-        orders[i] = or;
-        printf("Append: %d", orders[i]);
+        orders[i -1 ] = or -1;
+        // debug
+       // printf("Append: %d", orders[i]);
         printf("Wanna add more? ");
         // if 0 reciept
         if (or == 0) {
             receipt();
             break;
         } else {
-            i++;
-            printf("food is %s orders is %d", food[orders[i]], orders[i]);
+            // debug
+          //  printf("\nfood is %s orders is %d", food[orders[i -1]], orders[i-1]);
         }
     }
 }
@@ -76,9 +105,9 @@ void menu() {
 void delay(int ms) {
     long pe;
     clock_t now, then;
-    pe = ms * (CLOCKS_PER_SEC/1000);
+    pe = ms * (CLOCKS_PER_SEC / 1000);
     now = then = clock();
-    while((now-then) < pe) {
+    while ((now-then) < pe) {
         now = clock();
     }
 }
